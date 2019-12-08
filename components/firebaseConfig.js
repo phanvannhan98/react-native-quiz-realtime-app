f36ddc4a9c93ae48f538a523f529c93a1c78dfdd
LE TUNG KHANH <ltk1909@gmail.com>
2019-12-08 20:58:01 +0700

    config

---
+++ components/firebaseConfig.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyCNpbNd3O_1vo50eInktNYyR2IvGnIp0KA",
+    authDomain: "quizdanceapp.firebaseapp.com",
+    databaseURL: "https://quizdanceapp.firebaseio.com",
+    projectId: "quizdanceapp",
+    storageBucket: "quizdanceapp.appspot.com",
+    messagingSenderId: "200378895079",
+    appId: "1:200378895079:web:d634db51f79198dedcf042",
+    measurementId: "G-YPD3JRGWBS"
+  };
+  // Initialize Firebase
+ export default firebaseApp = firebase.initializeApp(firebaseConfig);
